fb526959670b4e43696f9e761d554c87e48972ff
Abdotarek442 <114480959+Abdotarek442@users.noreply.github.com>
2025-05-05 15:31:31 +0300

    Create GomokuPlayer.py
    Uploading GomokuPlayer

---
+++ GomokuPlayer.py
@@ -0,0 +1,12 @@
+class GomokuPlayer(Player):
+    def get_move() -> tuple[int, int]:
+        while True:
+            try:
+                move_input = input("Enter your move two numbers separated by space (row col, 0-14): ").strip()
+                i, j = map(int, move_input.split())
+                if 0 <= i < 15 and 0 <= j < 15:
+                    return (i, j)
+                else:
+                    print("Invalid input: numbers must be between 1 and 15")
+            except ValueError:
+                print("Invalid input: please enter two numbers separated by space")
